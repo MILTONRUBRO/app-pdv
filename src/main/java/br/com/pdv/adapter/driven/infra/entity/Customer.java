8bbc9c7dad2828d5533b667e0a47bc959f483286
Milton <miltonrubro@gmail.com>
2024-08-08 16:46:36 -0300

    Adicionando Swagger

--- src/main/java/br/com/pdv/adapter/driven/infra/entity/Customer.java
+++ src/main/java/br/com/pdv/adapter/driven/infra/entity/Customer.java
@@ -7,11 +7,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 
 @Entity
 @Getter
 @Setter
+@ToString
 public class Customer {
 	
     @Id
